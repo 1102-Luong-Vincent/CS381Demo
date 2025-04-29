485225199be1762b085d7624c3a8939d0cbaa05c
Vincent Luong <vinceluong2@gmail.com>
2025-04-29 16:42:19 -0700

    This is my as10 reattempt

--- as10/main_game.gd
+++ as10/main_game.gd
@@ -10,45 +10,54 @@ var value = 0
 func _ready():
 	increase_button.pressed.connect(_on_increase_pressed)
 	decrease_button.pressed.connect(_on_decrease_pressed)
-	update_label() # Set initial label text
+	update_label()
 	
+	await get_tree().create_timer(0.5).timeout  # <== Give time for peers to fully connect
 	update_player_list()
 
+	await get_tree().create_timer(1.0).timeout
+	update_player_list()
+	
 	multiplayer.peer_connected.connect(update_player_list)
 	multiplayer.peer_disconnected.connect(update_player_list)
 
 func update_label():
-	label.text = "Value: " + str(value)	
-	
-# Function which updates the above value
-func updateValue(newValue: int, from_owner = false):	
-	# If we don't own the node and the message is not from the owner
-	if !(is_multiplayer_authority() or from_owner):
-		# Ask the owner to update the value
-		rpc_id(get_multiplayer_authority(), "update_counter", newValue, false)
-		return
-	
-	value = newValue
-	update_label()
-	
-	# Make sure the value change gets propagated to all of the clients! 
-	if is_multiplayer_authority() and !from_owner:
-		rpc("update_counter", newValue, true)
+	label.text = "Value: " + str(value)
 
-# RPC which calls update value if called by the object's owner or the server
-@rpc("any_peer")
-func update_counter(new_value: int, from_owner: bool = false) -> void:
-	if is_multiplayer_authority() or from_owner:
-		updateValue(new_value, from_owner)
-
+# Called locally by anyone
 func _on_increase_pressed():
-	update_counter(value + 1)
+	if multiplayer.is_server():
+		request_update(1)  # call directly if host
+	else:
+		request_update.rpc_id(1, 1)  # send to host if client
 
 func _on_decrease_pressed():
-	update_counter(value - 1)
+	if multiplayer.is_server():
+		request_update(-1)
+	else:
+		request_update.rpc_id(1, -1)
+
+# Only the host receives this call
+@rpc("any_peer")
+func request_update(delta: int):
+	if multiplayer.is_server():
+		value += delta
+		broadcast_value.rpc(value)   # Send to all the clients
+		broadcast_value.call(value)  # Run locally on host
+
+# Sync the value on all peers
+@rpc("authority")
+func broadcast_value(new_value: int):
+	value = new_value
+	update_label()
 
 func update_player_list():
 	var ids = multiplayer.get_peers()
-	player_list_label.text = "Connected Player IDs:\n"
+	ids.insert(0, multiplayer.get_unique_id())  # Include self at top
+		
+	player_list_label.text = "Connected Player IDs: \n"
 	for id in ids:
-		player_list_label.text += "- ID: %s\n" % id
+		if id == multiplayer.get_unique_id():
+			player_list_label.text += "- ID: %s (You)\n" % id
+		else:	
+			player_list_label.text += "- ID: %s\n" % id
